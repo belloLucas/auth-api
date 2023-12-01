a490d608f26b9515dec90d8b0223a348d2556536
belloLucas <lucasbellodev@gmail.com>
2023-12-01 15:50:17 -0300

    feat: Index finished

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const homepage = require("./src/routes/homepage.js");
+const signup = require("./src/routes/signup.js");
+const signIn = require("./src/routes/signIn.js");
+const auththenticatedPage = require("./src/routes/authenticatedpage.js");
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.port;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+app.use("/", homepage);
+
+app.use("/auth/register", signup);
+
+app.use("/", signIn);
+
+app.use("/", auththenticatedPage);
+
+app.use((req, res) => {
+  res.status(404).json({ msg: "Página não encontrada." });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@auth.ayi1vyy.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`DB Connected`);
+    });
+  })
+  .catch((err) => console.log(err));
